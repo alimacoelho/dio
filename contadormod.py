d46f1bb9ffc7ea458d3c41408927257892b24181
alimacoelho <87615970+alimacoelho@users.noreply.github.com>
2021-07-28 22:56:06 -0300

    Update contadormod.py

--- contadormod.py
+++ contadormod.py
@@ -1,13 +1,22 @@
 import sys, re
+import txt
 from collections import Counter
 from pyspark import SparkContext, SparkConf
 if __name__ == "__main__":
     sc = SparkContext("local","PySpark Exemplo - Desafio Dataproc")
-    val words = sc.wholeTextFiles("gs://desafiodata/toystory1.txt")
-    words.toLocalIterator.mkString
-    words = re.sub(r'\(.+\)|<.+?>|\{.+\||\[.+?\]| \*.+?\* |\] | ! |\}', '', words)
     
-    wordlist = words.split()
+lines = []   
+with open(r'gs://desafiodata/toystory1.txt','r') as txtfile:
+    txtreader = txt.reader(txtfile)
+    for line in txtreader:
+        lines.append(line)
+    
+    
+    
+    lines.toLocalIterator.mkString
+    lines = re.sub(r'\(.+\)|<.+?>|\{.+\||\[.+?\]| \*.+?\* |\] | ! |\}', '', lines)
+    
+    wordlist = lines.split()
     wordcount = Counter(wordlist)
     result = str(wordcount.most_common(10))
     result = re.sub( '\[|]|\(| [),]', '', result)
