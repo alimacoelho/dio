adef72da60e4a1eb0b5f2194e419a8d9782117ea
alimacoelho <87615970+alimacoelho@users.noreply.github.com>
2021-07-28 22:10:11 -0300

    Update contadormod.py

--- contadormod.py
+++ contadormod.py
@@ -3,7 +3,7 @@ from collections import Counter
 from pyspark import SparkContext, SparkConf
 if __name__ == "__main__":
     sc = SparkContext("local","PySpark Exemplo - Desafio Dataproc")
-    words = sc.wholeTextFiles("gs://desafiodata/toystory1.txt")
+    words = str(sc.wholeTextFiles("gs://desafiodata/toystory1.txt"))
     words = re.sub(r'\(.+\)|<.+?>|\{.+\||\[.+?\]| \*.+?\* |\] | ! |\}', '', words)
     
     wordlist = words.split()
